92c82e3c83009e9513c1f3361991997a3ffa9ef1
summer <lijunjie@hniot.com>
2018-03-29 09:31:49 +0800

    SS

--- FTPDownLoad/Form1.Designer.cs
+++ FTPDownLoad/Form1.Designer.cs
@@ -30,11 +30,14 @@
 		{
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.button3 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(248, 28);
+			this.button1.Location = new System.Drawing.Point(325, 28);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(87, 29);
 			this.button1.TabIndex = 0;
@@ -44,18 +47,34 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(74, 28);
+			this.button2.Location = new System.Drawing.Point(30, 28);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(87, 29);
+			this.button2.Size = new System.Drawing.Size(104, 29);
 			this.button2.TabIndex = 1;
-			this.button2.Text = "文件";
+			this.button2.Text = "上传文件夹";
 			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(163, 28);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(104, 29);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "上传文件";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(434, 311);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
@@ -68,6 +87,9 @@
 
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
--- FTPDownLoad/Form1.cs
+++ FTPDownLoad/Form1.cs
@@ -14,204 +14,69 @@ namespace FTPDownLoad
 			InitializeComponent();
 		}
 
+
+		string localPath;
+		string fileName;
+		bool UploadFile = false;
 		private void button1_Click(object sender, EventArgs e)
 		{
-			string localPath = @"C:\Users\summer\Desktop\";
-			string fileName = "MODBUS协议中文版.pdf";
+
+			if (string.IsNullOrEmpty(localPath) || string.IsNullOrEmpty(fileName))
+			{
+				MessageBox.Show("路径不对");
+				return;
+			}
 
 			FileInfo fi = new FileInfo(localPath);
-			Ftp ftp = new Ftp();
-			//ftp.UploadDirectory(localPath, "ftp://192.168.0.15/", "四院报警上传文档");
-			ftp.UpLoadFile(localPath+fileName, "ftp://192.168.0.15/"+fileName);
+			FTPHelper ftp = new FTPHelper();
+			if (UploadFile)//文件
+			{
+				ftp.UpLoadFile(localPath, "ftp://192.168.0.15/" + fileName);
+			}
+			else
+			{
+				ftp.UploadDirectory(localPath, "ftp://192.168.0.15/", fileName);
+			}
+
+			localPath = "";
+			fileName = "";
+			UploadFile = false;
 		}
-	}
 
-	public class Ftp
-	{
-
-		private string ftpUserID = "784514076@qq.com";//用户名  
-		private string ftpPassword = "junjie19900320";//密码  
-
-		/// <summary>  
-		/// 上传文件  
-		/// </summary>  
-		/// <param name="localFile">要上传到FTP服务器的本地文件</param>  
-		/// <param name="ftpPath">FTP地址</param>  
-		public void UpLoadFile(string localFile, string ftpPath)
+		/// <summary>
+		/// 上传文件夹
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button2_Click(object sender, EventArgs e)
 		{
-			if (!File.Exists(localFile))
+			string path = "";
+			
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
 			{
-				MessageBox.Show("文件：“" + localFile + "” 不存在！");
-				return;
+				UploadFile = false;
+				path = folderBrowserDialog1.SelectedPath;
 			}
-			FileInfo fileInf = new FileInfo(localFile);
-			FtpWebRequest reqFTP;
 
-			reqFTP = (FtpWebRequest)FtpWebRequest.Create(ftpPath);// 根据uri创建FtpWebRequest对象   
-			reqFTP.Credentials = new NetworkCredential(ftpUserID, ftpPassword);// ftp用户名和密码  
-			reqFTP.KeepAlive = false;// 默认为true，连接不会被关闭 // 在一个命令之后被执行  
-			reqFTP.Method = WebRequestMethods.Ftp.UploadFile;// 指定执行什么命令  
-			reqFTP.UseBinary = true;// 指定数据传输类型  
-			reqFTP.ContentLength = fileInf.Length;// 上传文件时通知服务器文件的大小  
-			int buffLength = 2048;// 缓冲大小设置为2kb  
-			byte[] buff = new byte[buffLength];
-			int contentLen;
-
-			// 打开一个文件流 (System.IO.FileStream) 去读上传的文件  
-			FileStream fs = fileInf.OpenRead();
-			try
-			{
-				Stream strm = reqFTP.GetRequestStream();// 把上传的文件写入流  
-				contentLen = fs.Read(buff, 0, buffLength);// 每次读文件流的2kb  
-
-				while (contentLen != 0)// 流内容没有结束  
-				{
-					// 把内容从file stream 写入 upload stream  
-					strm.Write(buff, 0, contentLen);
-					contentLen = fs.Read(buff, 0, buffLength);
-				}
-				// 关闭两个流  
-				strm.Close();
-				fs.Close();
-				MessageBox.Show("文件【" + ftpPath + "/" + fileInf.Name + "】上传成功！<br/>");
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show("上传文件【" + ftpPath + "/" + fileInf.Name + "】时，发生错误：" + ex.Message + "<br/>");
-			}
+			string[] temp = path.Split('\\');
+			fileName = temp.Last();
+			localPath = path.Replace(fileName, "");
 		}
 
-
-
-		#region 上传文件夹  
-
-		/// <summary>  
-		/// 上传整个目录  
-		/// </summary>  
-		/// <param name="localDir">要上传的目录的上一级目录</param>  
-		/// <param name="ftpPath">FTP路径</param>  
-		/// <param name="dirName">要上传的目录名</param>  
-		/// <param name="ftpUser">FTP用户名（匿名为空）</param>  
-		/// <param name="ftpPassword">FTP登录密码（匿名为空）</param>  
-		public void UploadDirectory(string localDir, string ftpPath, string dirName)
-		{
-			string dir = localDir + dirName + @"\"; //获取当前目录（父目录在目录名）  
-													//检测本地目录是否存在  
-			if (!Directory.Exists(dir))
-			{
-				MessageBox.Show("本地目录：“" + dir + "” 不存在！<br/>");
-				return;
-			}
-			//检测FTP的目录路径是否存在  
-			if (!CheckDirectoryExist(ftpPath, dirName))
-			{
-				MakeDir(ftpPath, dirName);//不存在，则创建此文件夹  
-			}
-			List<List<string>> infos = GetDirDetails(dir); //获取当前目录下的所有文件和文件夹  
-
-			//先上传文件  
-			//Response.Write(dir + "下的文件数：" + infos[0].Count.ToString() + "<br/>");  
-			for (int i = 0; i < infos[0].Count; i++)
-			{
-				Console.WriteLine(infos[0][i]);
-				UpLoadFile(dir + infos[0][i], ftpPath + dirName + @"/" + infos[0][i]);
-			}
-			//再处理文件夹  
-			//Response.Write(dir + "下的目录数：" + infos[1].Count.ToString() + "<br/>");  
-			for (int i = 0; i < infos[1].Count; i++)
-			{
-				UploadDirectory(dir, ftpPath + dirName + @"/", infos[1][i]);
-				//Response.Write("文件夹【" + dirName + "】上传成功！<br/>");  
-			}
-		}
-
-		/// <summary>  
-		/// 判断ftp服务器上该目录是否存在  
-		/// </summary>  
-		/// <param name="ftpPath">FTP路径目录</param>  
-		/// <param name="dirName">目录上的文件夹名称</param>  
-		/// <returns></returns>  
-		private bool CheckDirectoryExist(string ftpPath, string dirName)
-		{
-			bool flag = true;
-			try
-			{
-				//实例化FTP连接  
-				FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(ftpPath + dirName);
-				ftp.Credentials = new NetworkCredential(ftpUserID, ftpPassword);
-				ftp.Method = WebRequestMethods.Ftp.ListDirectory;
-				FtpWebResponse response = (FtpWebResponse)ftp.GetResponse();
-				response.Close();
-			}
-			catch (Exception)
-			{
-				flag = false;
-			}
-			return flag;
-		}
-
-		/// <summary>  
-		/// 创建文件夹    
-		/// </summary>    
-		/// <param name="ftpPath">FTP路径</param>    
-		/// <param name="dirName">创建文件夹名称</param>    
-		public void MakeDir(string ftpPath, string dirName)
+		/// <summary>
+		/// 上传文件
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button3_Click(object sender, EventArgs e)
 		{
 
-			FtpWebRequest reqFTP;
-			try
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
 			{
-				string ui = (ftpPath + dirName).Trim();
-				reqFTP = (FtpWebRequest)FtpWebRequest.Create(ui);
-				reqFTP.Method = WebRequestMethods.Ftp.MakeDirectory;
-				reqFTP.UseBinary = true;
-				reqFTP.Credentials = new NetworkCredential(ftpUserID, ftpPassword);
-				FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
-				Stream ftpStream = response.GetResponseStream();
-				ftpStream.Close();
-				response.Close();
-				MessageBox.Show("文件夹【" + dirName + "】创建成功！<br/>");
+				UploadFile = true;
+				localPath = openFileDialog1.FileName;
+				fileName = localPath.Split('\\').Last();
 			}
-
-			catch (Exception ex)
-			{
-				MessageBox.Show("新建文件夹【" + dirName + "】时，发生错误：" + ex.Message);
-			}
-
 		}
-
-		/// <summary>  
-		/// 获取目录下的详细信息  
-		/// </summary>  
-		/// <param name="localDir">本机目录</param>  
-		/// <returns></returns>  
-		private List<List<string>> GetDirDetails(string localDir)
-		{
-			List<List<string>> infos = new List<List<string>>();
-			try
-			{
-				infos.Add(Directory.GetFiles(localDir).ToList()); //获取当前目录的文件  
-
-				infos.Add(Directory.GetDirectories(localDir).ToList()); //获取当前目录的目录  
-
-				for (int i = 0; i < infos[0].Count; i++)
-				{
-					int index = infos[0][i].LastIndexOf(@"\");
-					infos[0][i] = infos[0][i].Substring(index + 1);
-				}
-				for (int i = 0; i < infos[1].Count; i++)
-				{
-					int index = infos[1][i].LastIndexOf(@"\");
-					infos[1][i] = infos[1][i].Substring(index + 1);
-				}
-			}
-			catch (Exception ex)
-			{
-				ex.ToString();
-			}
-			return infos;
-		}
-
-		#endregion
 	}
 }
